daac35fffb4064a8e9914ba2bcf7e66473aa58fa
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2022-02-17 19:42:48 +0100

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -7,5 +7,5 @@ if [ -z "$1" ]; then runmin=256630; else runmin=$1; fi
 if [ -z "$2" ]; then runmax=260627; else runmax=$2; fi
 if [ -z "$3" ]; then cert=Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON_v2.txt; else cert=$3; fi
 
-brilcalc lumi -c web --begin $runmin --end $runmax -i $cert
+brilcalc lumi -c web --begin $runmin --end $runmax -i /mnt/vol/$cert
 
