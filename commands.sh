6bf0e7497f16c1e14be5ce0e4dcc8ff2e2c065fd
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2022-02-17 19:37:59 +0100

    Update commands.sh (#3)
    * Update commands.sh
    
    * Update info.yaml

--- commands.sh
+++ commands.sh
@@ -1,8 +1,11 @@
-#!/bin/sh -l
-# exit on erro
+#!/bin/bash -l
+# parameters: $1 
+# exit on error
 set -e
-# sudo chown $USER /mnt/vol
 
-brilcalc --help 
+if [ -z "$1" ]; then runmin=256630; else runmin=$1; fi
+if [ -z "$2" ]; then runmax=260627; else runmax=$2; fi
+if [ -z "$3" ]; then cert=Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON_v2.txt; else cert=$3; fi
 
-# cp *.out /mnt/vol/
+brilcalc lumi -c web --begin $runmin --end $runmax -i $cert
+
