a07985562fe6c9381b0659e1031468eb6246cb03
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2022-02-21 17:33:57 +0100

    Run range and byls option (#7)
    * add run ranges
    
    * env variables
    
    * pass parameters through files
    
    * byls option
    
    * fix option

--- commands.sh
+++ commands.sh
@@ -1,17 +1,25 @@
 #!/bin/bash -l
-# parameters: $1 
+# parameters: 
+#   $1 certified run json
+#   $2 output style csv or tab
+#   $3 summary or byls (if not byls, no option taken)
+#   $4 run range first
+#   $5 run range last
 # exit on error
 set -e
 
 cert=$1
-if [  -z "$2" ]
+style=$2
+if [ $3 == byls ]; then mode="--"$3; fi;
+
+if [  -z "$4" ]
 then
-  brilcalc lumi -c web -i /mnt/vol/$cert -u /fb --normtag /mnt/vol/inputs/normtag_PHYSICS.json
-elif [ -z "$3" ]
+  brilcalc lumi -c web $mode -i /mnt/vol/$cert -u /fb --normtag /mnt/vol/inputs/normtag_PHYSICS.json --output-style $style
+elif [ -z "$5" ]
 then
   echo "Give maximum range" 
 else
-  runmin=$2
-  runmax=$3
-  brilcalc lumi -c web --begin $runmin --end $runmax -i /mnt/vol/$cert -u /fb --normtag /mnt/vol/inputs/normtag_PHYSICS.json
+  runmin=$4
+  runmax=$5
+  brilcalc lumi -c web $mode --begin $runmin --end $runmax -i /mnt/vol/$cert -u /fb --normtag /mnt/vol/inputs/normtag_PHYSICS.json  --output-style $style
 fi
