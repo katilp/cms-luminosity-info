43dd7e74e265ceb1f6b19549dc3db3b258b16fcb
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2022-02-21 13:48:32 +0100

    Add normtag file

--- commands.sh
+++ commands.sh
@@ -6,12 +6,12 @@ set -e
 cert=$1
 if [  -z "$2" ]
 then
-  brilcalc lumi -c web -i /mnt/vol/$cert -u /fb --normtag /mnt/vol/normtag_PHYSICS.json
+  brilcalc lumi -c web -i /mnt/vol/$cert -u /fb --normtag /mnt/vol/inputs/normtag_PHYSICS.json
 elif [ -z "$3" ]
 then
   echo "Give maximum range" 
 else
   runmin=$2
   runmax=$3
-  brilcalc lumi -c web --begin $runmin --end $runmax -i /mnt/vol/$cert -u /fb --normtag /mnt/vol/normtag_PHYSICS.json
+  brilcalc lumi -c web --begin $runmin --end $runmax -i /mnt/vol/$cert -u /fb --normtag /mnt/vol/inputs/normtag_PHYSICS.json
 fi
