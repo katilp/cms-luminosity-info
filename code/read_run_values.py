541046e4b458f99f534dc8d34624245ce39037ef
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2022-02-21 20:01:54 +0100

    Create record (#8)
    * lumi record
    
    * update workflow

--- code/read_run_values.py
+++ code/read_run_values.py
@@ -3,28 +3,16 @@
 import subprocess
 import sys
 
+"""
+Read run range information from an input file.
+"""
+
 input = sys.argv[1]
 if "Run" in input:
     run_period = input
 else:
     year = input
 
-"""
-Create Run-2 luminosity records.
-"""
-
-RECID_START = 1055
-
-def read_run_periods(year):
-    """Read run periods for the given year."""
-
-    run_periods = []
-    with open("./inputs/run_ranges_run2.txt", "r") as f:
-        for line in f.readlines():
-            run_period = line.split(",")[0]
-            if year in run_period:
-                run_periods.append(run_period)
-    return run_periods
 
 
 def read_run_range(run_period):
