463a83a6e1a5218ae03c0a29e689ff94a5dabf5c
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2023-05-12 10:42:06 +0200

    sync to data-curation and add type to api query (#29)

--- cms-YYYY-luminosity/code/lumi_records.py
+++ cms-YYYY-luminosity/code/lumi_records.py
@@ -2,12 +2,10 @@
 
 import json
 import datetime
-import subprocess
 import requests
 
 import sys
 sys.path.insert(1, '../cms-release-info')
-from helpers import *
 
 """
 Create a luminosity record.
@@ -23,8 +21,8 @@ def create_record(recid, year, uncertainty, lumi_ref, val_recid):
 
     rec = {}
 
-    year_created = year
     year = str(year)
+    year_created = year
     year_published = datetime.date.today().strftime("%Y")
     # NB the reference needs to be to cds for this to work:
     url = lumi_ref+'/?of=tm&ot=245__a'
@@ -36,8 +34,8 @@ def create_record(recid, year, uncertainty, lumi_ref, val_recid):
     od_runs = json.loads(requests.get(url).text.strip())
 
     rec["abstract"]["description"] = (
-            "<p>CMS measures the luminosity using different luminometers (luminosity detectors) and algorithms. The luminometer giving the best value for each luminosity section is recorded in a 'normtag' file that is used in the luminosity calculation.</p>"
-            + "<p>The integrated luminosity for validated runs and luminosity sections of the %s public data (%s) is available in %slumi.txt (The integrated luminosity for validated runs and luminosity sections of all %s p-p data taking is available in %slumi.txt.)</p>" % (year, ",".join(od_runs),  ",".join(od_runs), year, year)
+            "<p>CMS measures the luminosity using different luminometers (luminosity detectors) and algorithms. The luminometer giving the best value for each luminosity section is recorded in a 'normtag' file <a href=\"/record/%s/files/normtag_PHYSICS_%s.json\">normtag_PHYSICS_%s.json</a> that is used in the luminosity calculation.</p>" % (recid, year, year)
+            + "<p>The integrated luminosity for validated runs and luminosity sections of the %s public data (%s) is available in %slumi.txt. (The integrated luminosity for validated runs and luminosity sections of all %s p-p data taking is available in %slumi.txt.)</p>" % (year, ",".join(od_runs), ",".join(od_runs), year, year)
             + "<p> For luminosity calculation, a detailed list of luminosity by lumi section is provided in <a href=\"/record/%s/files/%slumibyls.csv\">%slumibyls.csv</a> for the <a href=\"/record/%s\">list of validated runs</a> and lumi sections.</p>" % (recid, year, year, val_recid)
             + "<p>The uncertainty in the luminosity measurement of %s data should be considered as %s%% (reference <a href=\"%s\">%s</a>).</p>" % (year, uncertainty, lumi_ref, lumi_ref_title)
             + "<p>In your estimate for the integrated luminosity, check for which runs the trigger you have selected is active and sum the values for those runs. If you are using prescaled triggers, you can find the trigger prescale factors as shown in <a href=\"/record/5004\">the trigger examples</a>. The change of prescales (run, lumi section, index of prescales) is recorded in <a href=\"/record/%s/files/prescale%s.csv\">prescale%s.csv</a></p>" % (recid, year, year)
@@ -68,16 +66,24 @@ def create_record(recid, year, uncertainty, lumi_ref, val_recid):
     rec["license"]["attribution"] = "CC0"
 
     rec["links"] = {}
-    rec["links"]["url"] = lumi_ref
-    rec["links"]["title"] = lumi_ref_title
+    rec["links"] = [
+        {
+            "url": lumi_ref,
+            "title": lumi_ref_title
+        }
+    ]   
 
     rec["publisher"] = "CERN Open Data Portal"
 
     rec["recid"] = str(recid)
 
     rec["relations"] = {}
-    rec["relations"]["recid"] = val_recid
-    rec["relations"]["type"] = "isRelatedTo"
+    rec["relations"] = [
+        {
+            "recid": str(val_recid),
+            "type": "isRelatedTo"
+        }
+    ]
 
     url = 'http://api-server-cms-release-info.app.cern.ch/runeras/run_era?year='+year+'&type=pp-phys'
     #rec["run_period"] = read_run_periods(year, 'pp-phys')
@@ -113,7 +119,7 @@ def main():
     # this_year = all_years[year]
 
     # this gets json from the api server
-    url = 'http://api-server-cms-release-info.app.cern.ch/years?year='+year+'&output=plain'
+    url = 'http://api-server-cms-release-info.app.cern.ch/years?year='+year+'&type=pp&&output=plain'
     this_year = json.loads(requests.get(url).text.strip())
     
     records.append(
