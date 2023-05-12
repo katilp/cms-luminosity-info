be342a0e299bdd918dc7eb7c720f051735d78217
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2023-05-12 17:37:09 +0200

    add option for norm tag or pxl

--- cms-YYYY-luminosity/commands.sh
+++ cms-YYYY-luminosity/commands.sh
@@ -2,24 +2,34 @@
 # parameters: 
 #   $1 certified run json
 #   $2 output style csv or tab
-#   $3 summary or byls (if not byls, no option taken)
-#   $4 run range first
-#   $5 run range last
+#   $3 use normtag file or pxl
+#   $4 summary or byls (if not byls, no option taken)
+#   $5 run range first
+#   $6 run range last
 # exit on error
 set -e
 
 cert=$1
 style=$2
-if [ "$3" == "byls" ]; then mode="--"$3; fi;
 
-if [  -z "$4" ]
+if [ "$3" == "normtag" ]
+then 
+  option="--normtag /mnt/vol/normtag_PHYSICS.json"
+elif [ "$3" == "pxl" ]
 then
-  brilcalc lumi -c web $mode -i /mnt/vol/$cert -u /fb --normtag /mnt/vol/normtag_PHYSICS.json --output-style $style
-elif [ -z "$5" ]
+  option="--type pxl"
+fi  
+
+if [ "$4" == "byls" ]; then mode="--"$4; fi;
+
+if [  -z "$5" ]
+then
+  brilcalc lumi -c web $mode -i /mnt/vol/$cert -u /fb $option --output-style $style
+elif [ -z "$6" ]
 then
   echo "Give maximum range" 
 else
-  runmin=$4
-  runmax=$5
-  brilcalc lumi -c web $mode --begin $runmin --end $runmax -i /mnt/vol/$cert -u /fb --normtag /mnt/vol/normtag_PHYSICS.json  --output-style $style
+  runmin=$5
+  runmax=$6
+  brilcalc lumi -c web $mode --begin $runmin --end $runmax -i /mnt/vol/$cert -u /fb $option  --output-style $style
 fi
